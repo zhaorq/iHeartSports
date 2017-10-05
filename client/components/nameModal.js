7d5483ac48939a9dbc323d3c5c5cc39095d949a5
zhaorq <zhaorq@gmail.com>
2017-10-05 16:20:53 -0700

    [update] Added nameModal to achieve a pop-up effect

--- client/components/nameModal.js
+++ client/components/nameModal.js
@@ -1,7 +1,32 @@
 import React from 'react';
 
 const NameModal = (props) => (
-  <div>Name Modal</div>
+  <div>
+    <div>
+      <button onClick={props.closeModal}>Close</button>
+      <h3>Edit Name</h3>
+    </div>
+    <div>
+      <label>
+        <span>First Name:</span>
+        <input type="text" defaultValue={props.name.first} onChange={e => props.setNameState({ first: e.target.value })} />
+      </label>
+      <label>
+        <span>Last Name:</span>
+        <input type="text" defaultValue={props.name.last} onChange={e => props.setNameState({ last: e.target.value })} />
+      </label>
+    </div>
+    <div>
+      <button onClick={props.closeModal}>Cancel</button>
+      <button
+        onClick={() => {
+          props.updateNameFromModal();
+          props.closeModal();
+        }}>
+        Save
+      </button>
+    </div>
+  </div>
 );
 
 export default NameModal;
