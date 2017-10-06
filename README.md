352f64dcf1c700fe52c30ef6a2a3cbb867569d4a
zhaorq <zhaorq@gmail.com>
2017-10-06 07:41:50 -0700

    [refactor] Added comment to the server routes

--- README.md
+++ README.md
@@ -1,13 +1,28 @@
 # iHeartSports
 
-### Requirements
+## Requirements
 
 Node 6.10.x
 
-### Start App
+## Development
+
+### Installing Dependencies
+
 From within the root directory:
 
+```sh
+npm install
+
+```
+
+### Start App
+
+From within the root directory:
+
+```sh
 npm start
+
+```
 
 ### Developer
 Tianyi Zhao
--- server/routes.js
+++ server/routes.js
@@ -5,12 +5,17 @@ const fs = require('fs');
 routes.get('/data', (req, res) => {
   fs.readFile(path.join(__dirname, './seed.json'), 'utf8', (err, data) => {
     if (err) throw err;
-    if (data === "") data = {};
+    if (data === "") data = {}; //start off a JSON file with at least an empty object
     res.status(200).send(JSON.stringify(data));
   });
 });
 
 routes.post('/data', (req, res) => {
+  /*When user edit the content, we want to keep the part in the existing file that didn't
+  change and update with the new entries. Hence, here we read file and take the existing 
+  data, merge with the new data (=req.body) through the Object.assign function, handle error,
+  or send a success status. 
+  */
   fs.readFile(path.join(__dirname, './seed.json'), 'utf8', (err, data) => {
     if (err) throw err;
     data = data === "" ? {} : JSON.parse(data);
