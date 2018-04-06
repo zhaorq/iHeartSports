b85900db8ae80e5f9913f4b705fd3566c8992c15
zhaorq <zhaorq@gmail.com>
2018-04-05 21:52:36 -0700

    try travis

--- client/components/app.js
+++ client/components/app.js
@@ -37,7 +37,7 @@ class App extends Component {
     return (
       <div>
         <div id="header">
-          <h1>Sports Magazine</h1>
+          <h1>iHeartSport</h1>
         </div>
         <div id="content">
         <h2>Sports Magazine Settings</h2>
