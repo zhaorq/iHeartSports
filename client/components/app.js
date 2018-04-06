4ed3ee0c682eec60ee8a0c364a7e625164f56778
zhaorq <zhaorq@gmail.com>
2018-04-05 22:27:50 -0700

    try auto deploy again

--- client/components/app.js
+++ client/components/app.js
@@ -37,7 +37,7 @@ class App extends Component {
     return (
       <div>
         <div id="header">
-          <h1>XXXXX</h1>
+          <h1>iheartsport</h1>
         </div>
         <div id="content">
         <h2>Sports Magazine Settings</h2>
